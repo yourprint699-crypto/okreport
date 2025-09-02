c71b4c4e8b23b40a55299e4248d14c0412849d5a
yourprint699-crypto <yourprint699@gmail.com>
2025-09-02 17:02:49 +0530

    Updated HomeHeroText.jsx

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -1,1 +1,24 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+import React from 'react'
+import Video from './Video'
+
+const HomeHeroText = () => {
+    return (
+        <div className='font-[font1] mt-72 lg:mt-0 pt-5 text-center relative z-20'>
+            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
+                L'étincelle
+            </div>
+            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-start uppercase lg:leading-[8vw] leading-[10vw]'>
+                qui
+                <div className='h-[7vw] w-[16vw] rounded-full -mt-3 overflow-hidden mx-2'>
+                    <Video />
+                </div>
+                génère
+            </div>
+            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
+                la créativité
+            </div>
+        </div>
+    )
+}
+
+export default HomeHeroText
